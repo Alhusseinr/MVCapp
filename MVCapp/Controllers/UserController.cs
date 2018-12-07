4ea8a75a13352257e0073f9c0779185d4169f1c2
Rami Alhussein <Alhusseinr1@montlciar.edu>
2018-12-07 11:13:37 -0500

    adding db and login

---
+++ MVCapp/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCapp.Models;
+
+namespace MVCapp.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude = "IsEmailVerified, ActivationCode")] User user)
+        {
+            bool Status = false;
+            string message = "";
+
+            //Model Validation
+            if (ModelState.IsValid)
+            {
+                #region //Email is on db already
+                var isExist = IsEmailExist(user.EmailID);
+                if (isExist)
+                {
+                    ModelState.AddModelError("EmailExist", "Email already exists");
+                    return View(user);
+                }
+                #endregion
+
+                #region Generate Activation Code 
+                user.ActivationCode = Guid.NewGuid();
+                #endregion
+
+                #region
+                //Password Hashing
+                user.Password = Crypto.Hash(user.Password);
+                user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword);
+                #endregion
+
+                user.IsEmailVerified = false;
+
+                #region Save to Database
+
+                using (DbEntities dc = new DbEntities())
+                {
+                    
+                }
+
+                #endregion
+            }
+        }
+    }
+}
---
+++ MVCapp/Crypto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace MVCapp
+{
+    public class Crypto
+    {
+        public static string Hash(string value)
+        {
+            return Convert.ToBase64String(System.Security.Cryptography.SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(value)));
+        }
+    }
+}
